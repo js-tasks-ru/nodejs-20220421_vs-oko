b96d3aae7a9f4c4dc6185971b7184971f39b35f5
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-02 23:55:26 +0300

    задача 03.02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,13 +4,26 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.data = '';
   }
 
   _transform(chunk, encoding, callback) {
+    this.data += chunk;
+    const dataStr = this.data.toString();
+    if (dataStr.includes(os.EOL)) {
+      const dataArr = dataStr.split(os.EOL).slice(0, -1);
+      dataArr.forEach((chunk) => this.push(chunk));
+      this.data = dataStr.split(os.EOL).slice(-1)[0];
+    }
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.data);
+    this._destroy(null, callback);
   }
 }
 
 module.exports = LineSplitStream;
+
+
