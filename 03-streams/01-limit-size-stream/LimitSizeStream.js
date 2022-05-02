45da4a1348c924224ac8ee3f23623241df708243
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-02 19:07:51 +0300

    задача 03.01

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,17 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit ? options.limit : null;
+    this.limitCounter = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.limitCounter += Buffer.byteLength(chunk);
+    let error = null;
+    if (this.limit && this.limitCounter > this.limit) {
+      error = new LimitExceededError();
+    }
+    callback(error, chunk);
   }
 }
 
