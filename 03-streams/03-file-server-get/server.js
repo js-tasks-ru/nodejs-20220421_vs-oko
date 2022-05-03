3d675f06cc793dcc49d6ea3687ee4e5a9a9ef5e7
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-03 20:19:34 +0300

    задача 03.03

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,6 +1,6 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,7 +8,26 @@ server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('nested files are not supported');
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
+  const stream = fs.createReadStream(filepath);
+
+  stream.on('error', (error) => {
+    if (error.code === 'ENOENT') {
+      res.statusCode = 404;
+      res.end('no such file');
+    } else {
+      res.statusCode = 500;
+      res.end('Something went wrong');
+    }
+  });
+
+  stream.pipe(res);
+  res.on('aborted', () => stream.destroy());
 
   switch (req.method) {
     case 'GET':
