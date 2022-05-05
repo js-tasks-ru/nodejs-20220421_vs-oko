a58a74be191625c050889826b525e238afa74eaa
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-05 17:22:04 +0300

    задача 03.04 (все тесты не проходит ошибку не нашел)

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,14 +1,45 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
-
+  if (pathname.includes('/')) {
+    res.statusCode = 400;
+    res.end('nested files are not supported');
+  }
   const filepath = path.join(__dirname, 'files', pathname);
+  fs.stat(filepath, (err) => {
+    if (err == null) {
+      res.statusCode = 409;
+      res.end('File exists');
+    } else if (err.code === 'ENOENT') {
+      const limitedStream = new LimitSizeStream({limit: 1e6});
+      limitedStream.on('error', (err) => {
+        if (err.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end(err.code);
+        } else {
+          res.statusCode = 500;
+          res.end('Something went wrong');
+        }
+      });
+      req.on('aborted', (err) => {
+        if (err) throw err;
+        console.log('file.txt was deleted');
+        outStream.destroy();
+        limitedStream.destroy();
+      });
+      const outStream = fs.createWriteStream(filepath);
+      req.pipe(limitedStream).pipe(outStream);
+    } else {
+      console.log('Some other error: ', err.code);
+    }
+  });
 
   switch (req.method) {
     case 'POST':
