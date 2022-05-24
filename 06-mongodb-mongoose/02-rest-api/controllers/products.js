09a6dd86bc980aab89919aa7657b1ca9742a75f0
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-24 18:50:17 +0300

    Задача 06.02

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,23 @@
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product.js')
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
+  const productsArr = await Product.find({subcategory})
 
-  ctx.body = {};
+  ctx.body = {products: productsArr.map(elem => mapProduct(elem))};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const productsArr = await Product.find()
+
+  ctx.body = {products: productsArr.map(elem => mapProduct(elem))};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const product = await Product.findById(ctx.params.id);
+  if (product) ctx.body = {product: mapProduct(product)};
 };
 
