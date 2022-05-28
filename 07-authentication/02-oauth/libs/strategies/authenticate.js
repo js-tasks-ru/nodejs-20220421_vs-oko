75cf39e6a8ac7f916eb68921c44d3001d31519de
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-28 16:06:26 +0300

    Задача 07.02

--- 07-authentication/02-oauth/libs/strategies/authenticate.js
+++ 07-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,25 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User.js');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+
+  try{
+
+    if ( !email ) return done(null, false, 'Не указан email');
+
+    let user = await User.findOne({email});
+
+    if ( !user ) {
+      user = await User.create({
+        email,
+        displayName,
+      });
+
+    }
+    return done(null, user);
+
+  } catch (err) {
+
+    return done(err)
+
+  }
 };
