966a155e5cbb7e7fd860496e06afd91e715b77c9
Aleksei Belousov <vs.oko@rambler.ru>
2022-05-30 19:13:16 +0300

    Задача 08.01

--- 08-email/01-registration/controllers/registration.js
+++ 08-email/01-registration/controllers/registration.js
@@ -3,9 +3,52 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  try {
+    const token = uuid();
 
+    const user = await new User({
+      email: ctx.request.body.email,
+      displayName: ctx.request.body.displayName,
+      verificationToken: token
+    });
+    await user.setPassword(ctx.request.body.password);
+    await user.save();
+
+    await sendMail({
+      template: 'confirmation',
+      locals: {token},
+      to: user.email,
+      subject: 'Подтвердите почту',
+
+    })
+    ctx.body = {"status": "ok"}
+
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
 };
 
 module.exports.confirm = async (ctx, next) => {
+  try {
+    const verificationToken = ctx.request.body.verificationToken;
 
+    const user = await User.findOne({verificationToken});
+    if(!user) {
+      console.log('пользователь с токеном', verificationToken, 'не найден');
+      ctx.status = 400;
+      ctx.body = {error: 'Ссылка подтверждения недействительна или устарела'};
+      return;
+    }
+    user.verificationToken = undefined;
+    await user.save();
+
+    await ctx.login(user);
+
+    ctx.body = {token: verificationToken}
+
+  } catch (err) {
+    throw err;
+  }
 };
+
